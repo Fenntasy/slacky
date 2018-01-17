35deefd1ac7394c25adc59ec7929fbc512e3b598
Vincent Billey <vincent.billey@gmail.com>
2018-01-17 17:43:40 +0100

    refactor with redux

--- src/App.js
+++ src/App.js
@@ -2,52 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 import Login from "./Login";
 import Chat from "./Chat";
+import { connect } from "react-redux";
+import { init } from "./websocket"
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userName: null,
-      messages: [],
-    };
-    // Attaching the websocket to our App so we can reuse it
-    this.websocket = new WebSocket(`ws://${window.location.hostname}:8080`);
-  }
   componentDidMount() {
-    // Listen for messages
-    this.websocket.addEventListener("message", event => {
-      const message = JSON.parse(event.data);
-      console.log("Message from server ", message);
-      switch (message.type) {
-        case "CONNECTION_START":
-        default:
-          break;
-        case "MESSAGES":
-          this.setState({ messages: message.data });
-          break;
-      }
-    });
+    init();
   }
-
-  handleUserName = userName => {
-    this.setState({ userName: userName });
-    this.websocket.send(
-      JSON.stringify({
-        type: "LOGIN",
-        userName: userName
-      })
-    );
-  };
-
-  sendMessage = message => {
-    this.websocket.send(
-      JSON.stringify({
-        type: "NEW_MESSAGE",
-        userName: this.state.userName,
-        message: message
-      })
-    );
-  };
 
   render() {
     return (
@@ -56,13 +17,19 @@ class App extends Component {
           <h1 className="App-title">Slacky</h1>
         </header>
 
-        {this.state.userName
-          ? (<Chat sendMessage={this.sendMessage} messages={this.state.messages} />
+        {this.props.userName
+          ? (<Chat />
           )
-          : (<Login handleUserName={this.handleUserName} />)}
+          : (<Login />)}
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    userName: state.userName
+  }
+}
+
+export default connect(mapStateToProps)(App);
