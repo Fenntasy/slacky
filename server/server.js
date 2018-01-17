4c9165c320d16ab1a27bb7f4cbe3ab519fd06cc4
Vincent Billey <vincent.billey@gmail.com>
2018-01-17 13:48:15 +0100

    basis for slacky

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const server = require("http").createServer();
+const app = express();
+
+// This will store the messages for the time of the session
+const messages = [];
+
+// Launch the Websocket server
+const WebsocketServer = new WebSocket.Server({ server: server });
+
+WebsocketServer.on("connection", function connection(ws, req) {
+  ws.on("message", function incoming(data) {
+    const message = JSON.parse(data);
+    switch (message.type) {
+      case "LOGIN":
+        ws.send(JSON.stringify({ type: "MESSAGES", data: messages}));
+        return;
+      case "NEW_MESSAGE":
+        // Add the message to the list of messages
+        messages.push({ userName: message.userName, message: message.message });
+
+        // Sends all messages to all connected clients
+        WebsocketServer.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: "MESSAGES", data: messages}));
+          }
+        });
+        return;
+    }
+  });
+
+  // Display an error when one occurs
+  // The most likely candidate is when a browser closes without closing
+  // the connection properly
+  ws.on("error", console.warn);
+});
+
+
+server.on("request", app);
+server.listen(8080, function listening() {
+  console.log("Listening on ", server.address().port);
+});
